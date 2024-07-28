0abb037cb0000a2f04497d232ac10c0709470210
Keerthigarayar <keerthigarayar@gmail.com>
2024-07-28 15:27:11 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const translateButton = document.querySelector("button[type='submit']");
+    
+    translateButton.addEventListener("click", function() {
+        // You can add any additional JavaScript functionality here if needed
+        console.log("Translate button clicked");
+    });
+});
