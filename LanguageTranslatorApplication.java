13b91f8d3e67b431536004cbae6f2825fd68588f
Keerthigarayar <keerthigarayar@gmail.com>
2024-07-28 15:08:38 +0530

    Create LanguageTranslatorApplication.java

---
+++ LanguageTranslatorApplication.java
@@ -0,0 +1,11 @@
+package com.example.translator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageTranslatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LanguageTranslatorApplication.class, args);
+    }
+}
